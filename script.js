18d11157d87cdc0099d1e88e7b7cf539bd40f7ae
Romraev <romraev@yandex.ru>
2024-05-21 20:42:02 +0200

    hw3

--- script.js
+++ script.js
@@ -1,29 +1,38 @@
-// Задание 1. Создать переменные num1, num2 которые пользователь вводит с клавиатуры. Проверьте, что переменная num1 равна или меньше 1, а переменная num2 больше или равна 3.
-[num1, num2] = prompt("Введите num1 и num2 через пробел").split(" ");
-if (num1 && num2) {
-  alert(`
-    num1 ${+num1 <= 1 ? "меньше либо равно 1" : "больше 1"}
-    num2 ${+num2 >= 3 ? "больше либо равно 3" : "меньше 3"}
-`);
-}else {
-    alert("Ошибка ввода!")
+// Задание 1. Задание 1. Создайте функцию которая возводит переданное число в куб, необходимо вывести в консоль результат 2^3 степени + 3 ^ 3 степени
+function getCube(num) {
+    return (+num) ** 3
 }
-// Задание 2. Перепишите код к тернарному оператору
-let test = true;
-console.log(test === true ? '+++':'---');
-// Задание 3. В переменной day лежит какое-то число из интервала от 1 до 31. Определите в какую декаду месяца попадает это число (в первую, вторую или третью).
+console.log(getCube(2) + getCube(3))
+
+// Задание 2. Пользователь вводит с клавиатуры число, если ввёл текст, необходимо вывести что значение задано неверно. Создать фукнцию, которая высчитывает 13% от данного числа и выводит в консоль текст "Размер заработной платы за вычетом налогов равен значение"
 const input1 = document.querySelector("#num1")
 const answer1 = document.querySelector(".answer1")
+function getSalary(income) {
+    return Number.isNaN(income) ? `Неверное значение`:`Размер заработной платы за вычетом налогов равен: ${income*0.87} у.е.`
+}
 
 input1.addEventListener('input', (event) => {
-    let day = event.target.value
-    answer1.innerHTML = `Это день ${+day < 30 ? Math.floor(+day/10 + 1) : 3} декады месяца`
+    let income = +event.target.value
+    answer1.innerHTML = getSalary(income)
 })
-// Задание 4. Необходимо от пользователя получить число. Необходимо вывести разряды числа, а именно: количество сотен, десятков и единиц.
-const input2 = document.querySelector("#num2")
-const answer2 = document.querySelector(".answer2")
 
-input2.addEventListener('input', (event) => {
-    let num = +event.target.value
-    answer2.innerHTML = `В числе ${num} количество сотен: ${Math.floor(num/100)}, десятков: ${Math.floor((num%100)/10)}, единиц: ${Math.floor(num%10)}`
-})
+// Задание 3. Пользователь с клавиатуры вводит 3 числа, необходимо создать функцию, которая определяет максимальное значение среди этих чисел.
+function getMax() {
+    let max = prompt("Введите числа через пробел").split(" ").reduce((a,b) => Math.max(+a, +b))
+    return Number.isNaN(max) ? "Неверный ввод":`Максимальное число: ${max}`
+}
+alert(getMax())
+
+// Задание 4. Необходимо от пользователя получить число. Необходимо вывести разряды числа, а именно: количество сотен, десятков и единиц.
+function calc(a, b, operation) {
+    switch(operation) {
+        case '+': return a+b
+        case '-': return Math.abs(a-b)
+        case '*': return a*b
+        case '/': return a/b
+    }
+}
+console.log(calc(2,5,'+'))
+console.log(calc(2,5,'-'))
+console.log(calc(2,5,'*'))
+console.log(calc(2,5,'/'))
